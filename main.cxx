64e88f97885a44c8617737d647aaa394ce1dc728
Vinzenz Petr <vinzenz_82@yahoo.de>
2025-01-03 19:42:15 +0100

    small mods

--- main.cxx
+++ main.cxx
@@ -4,7 +4,7 @@
 
 int main() {
     for(int i=0; i<10; i++) {
-        std::cout << "Hello Poky World!" << std::endl;
+        std::cout << "Hello \t Poky \t World!" << std::endl;
     }
     return 0;
 }
