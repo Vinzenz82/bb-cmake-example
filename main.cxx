8d6825f44399e84993d37e69d6cec6a64f2a4e9a
Vinzenz Petr <vinzenz_82@yahoo.de>
2025-01-19 12:17:34 +0100

    improved example code

--- main.cxx
+++ main.cxx
@@ -1,10 +1,38 @@
-// Your First C++ Program
-
 #include <iostream>
+#include <thread>
+#include <chrono>
+#include <mutex>
+
+std::mutex g_pages_mutex;
+
+void task1(std::string msg)
+{
+    std::lock_guard<std::mutex> guard(g_pages_mutex);
+    std::cout << "Task1 says: " << std::endl;
+    for (std::size_t x = 0, length = msg.length(); x != length; ++x) {
+        std::cout << msg[x] << std::endl;
+        std::this_thread::sleep_for(std::chrono::milliseconds(288));
+    }
+}
+
+void task2(std::string msg)
+{
+    std::lock_guard<std::mutex> guard(g_pages_mutex);
+    std::cout << "Task2 says: " << std::endl;
+    for (std::size_t x = 0, length = msg.length(); x != length; ++x) {
+        std::cout << msg[x] << std::endl;
+        std::this_thread::sleep_for(std::chrono::milliseconds(344));
+    }
+}
 
 int main() {
-    for(int i=0; i<10; i++) {
-        std::cout << "Hello \t Poky \t World!" << std::endl;
+    std::cout << "Hello Poky World!" << std::endl;
+
+    while(true) {
+        std::thread t1(task1, "PING PINg PIng Ping ping");
+        std::thread t2(task2, "Pong");
+        t1.join();
+        t2.join();
     }
     return 0;
 }
